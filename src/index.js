9817c1113acd539d29587c93233714ee3dc3cb2e
Alex Puschinsky <alexpoo@gmail.com>
2015-07-31 20:33:03 +0300

    Create Bootstrap code

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import rugRide from "./rug_ride"
+
+window.onload = () => {
+  rugRide({
+    domContainer: "#game-container",
+    width: 800,
+    height: 600
+  });
+}
---
+++ src/rug_ride.js
@@ -0,0 +1,8 @@
+import { Game } from "gamebone"
+
+export default function rugRide(options = {}) {
+  let game = game = new Game();
+
+  game.frame = (dt) => {}
+  game.start();
+}
