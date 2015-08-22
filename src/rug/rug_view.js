36af58d3c8bb87a9e5f8cb91a921f9d032cd9332
Alex Puschinsky <alexpoo@gmail.com>
2015-08-22 19:46:25 +0300

    Fix views drawing
    Adjust rectangles according to the top left point of body

--- src/rug/rug_view.js
+++ src/rug/rug_view.js
@@ -10,7 +10,7 @@ export default class ObstacleView extends View{
     let rugGraphics = new PIXI.Graphics()
     rugGraphics.lineStyle(0);
     rugGraphics.beginFill(0x00ffff, 1);
-    rugGraphics.drawRect(0, 0, this.model.width, this.model.height)
+    rugGraphics.drawRect(-this.model.width/2, -this.model.height/2, this.model.width, this.model.height);
     rugGraphics.endFill();
 
     this.container.addChild(rugGraphics);
